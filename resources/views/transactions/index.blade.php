fe5cbe830ed7b4298e0d00b2f03ec36a12dc2594
PraNAtaP <pranatapu08@gmail.com>
2026-09-23 21:10:49 +0700

    tampilkan jumlah total item per transaksi

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -8,9 +8,10 @@
 @foreach ($transactions as $transaction)
 <div class="border rounded-md p-3 mb-3">
     <p class="font-medium">
-        Transaksi #{{ $transaction->id }} &middot;
-        {{ $transaction->created_at->format('d M Y H:i') }} &middot;
-        Rp {{ number_format($transaction->total) }}
+        Transaksi #{{ $transaction->id }} 
+        &middot; {{ $transaction->created_at->format('d M Y H:i') }}
+        &middot; Rp {{ number_format($transaction->total) }}
+        &middot; {{ $transaction->details->sum('qty') }} item
     </p>
     <ul class="text-sm text-slate-500 mt-1">
         @foreach ($transaction->details as $detail)
