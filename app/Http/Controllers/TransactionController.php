55ae65ace11791c5ac70bcc31cfab71506596132
Nety Sulistyorini <sodachocolate719@gmail.com>
2026-09-23 15:54:26 +0700

    tambah pagination pada grid produk pos

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -8,11 +8,11 @@ use App\Models\Transaction;
 
 class TransactionController extends Controller
 {
-    public function create()
-    {
-        $products = Product::where('stock', '>', 0)->get();
-        return view('pos.create', ['products' => $products]);
-    }
+public function create()
+{
+    $products = Product::where('stock', '>', 0)->paginate(12);
+    return view('pos.create', ['products' => $products]);
+}
 
     public function store()
     {
